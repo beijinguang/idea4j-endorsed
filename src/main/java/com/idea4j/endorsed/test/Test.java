24f1879a6970e9b82df14b52f4e87ae2b971bdb0
beijinguang <wangjinguang66@163.com>
2020-09-11 13:26:07 +0800

    Initial commit

---
+++ src/main/java/com/idea4j/endorsed/test/Test.java
@@ -0,0 +1,15 @@
+package com.idea4j.endorsed.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * -Djava.endorsed.dirs=D:\idea4j-endorsed\endorsed -XX:+TraceClassLoading
+ */
+public class Test {
+    public static void main(String[] args) {
+//        List list = new ArrayList();
+        System.out.println(1);
+
+    }
+}
